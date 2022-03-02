8b9e4786b10d232dd5519c2d1f2bfcf84938038a
Kristina-VCyber <hatkova@inbox.ru>
2022-03-02 22:34:14 +0300

    Initial Commit

---
+++ src/Todolist.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+type TodolistPropsType = {
+    title: string;
+    task: Array<TaskPropsType>
+
+}
+type TaskPropsType = {
+    id: number,
+    title: string,
+    isDone: boolean
+}
+
+
+export const Todolist = (props: TodolistPropsType) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.task.map((el) => {
+                        debugger
+                        return (
+                            <li><input type="checkbox" checked={el.isDone}/>
+                                <span>{el.title}</span>
+                                {/*<button onClick={() => { alert('click')}}>x</button>*/}
+                            </li>
+                        )
+                    }
+                )}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
+
+
+
+
